a6a40a34dce1a5588cf568a7043504abd89c4482
Dawid Spisak <dawidspisak@gmail.com>
2021-08-24 12:51:39 +0200

    import bugfix

--- src/_metronic/layout/components/addProject/Contractor/training/Training.js
+++ src/_metronic/layout/components/addProject/Contractor/training/Training.js
@@ -1,6 +1,7 @@
 import { Button, makeStyles } from "@material-ui/core";
 import React, { useEffect } from "react";
 import { useState } from "react";
+import db from "../../../../../../firebase";
 import {
   Form,
   FormDateContainer,
@@ -39,9 +40,9 @@ const data = {
 
 const Training = () => {
   const classes = useStyles();
-  const [szkolenie, setPosrednictwo] = useState([]);
-  const [nazwaSzkolenia, setDataZakonczeniaSzkolenia] = useState("");
-  const [numerGrupy, setnumerGrupy] = useState("");
+  const [szkolenie, setSzkolenie] = useState([]);
+  const [nazwaSzkolenia, setNazwaSzkolenia] = useState("");
+  const [numerGrupy, setNumerGrupy] = useState("");
 
   const [nazwaFrimySzkolenia, setNazwaFrimySzkolenia] = useState("");
   const [kodPocztowySzkolenia, setKodPocztowySzkolenia] = useState("");
@@ -68,99 +69,241 @@ const Training = () => {
   const [terminSzkoleniaOd, setTerminSzkoleniaOd] = useState("");
   const [terminSzkoleniaDo, setTerminSzkoleniaDo] = useState("");
   const [rekomendacje, setRekomendacje] = useState("");
+
+  useEffect(() => {
+    db.collection("projects")
+      .doc(`projekt`)
+      .collection("wykonawcy")
+      .doc("szkoelnia")
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          setSzkolenie(doc.data());
+        } else {
+          console.log("No such document!");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    db.collection("projects")
+      .doc(`projekt`)
+      .collection("wykonawcy")
+      .doc("szkoelnia")
+      .set({
+        nazwaSzkolenia,
+        numerGrupy,
+        nazwaFrimySzkolenia,
+        kodPocztowySzkolenia,
+        miastoSzkolenia,
+        ulicaSzkolenia,
+        numerMieszkaniaSzkolenia,
+        numerUmowySzkolenia,
+        dataUmowySzkolenia,
+        umowaPowierdzaniaDanych,
+        umowaZgloszonaWup,
+        dataZgloszenia,
+        nazwaFrimyPodwykonawcy,
+        kodPocztowyPodwykonawcy,
+        miastoPodwykonawcy,
+        ulicaPodwykonawcy,
+        numerMieszkaniaPodwykonawcy,
+        trener,
+        egzaminator,
+        terminSzkoleniaOd,
+        terminSzkoleniaDo,
+        rekomendacje,
+      });
+    console.log("dodano");
+  };
+
+  useEffect(() => {
+    if (szkolenie) {
+      setNazwaSzkolenia(szkolenie.nazwaSzkolenia || "");
+      setNumerGrupy(szkolenie.numerGrupy || "");
+      setNazwaFrimySzkolenia(szkolenie.nazwaFrimySzkolenia || "");
+      setKodPocztowySzkolenia(szkolenie.kodPocztowySzkolenia || "");
+      setMiastoSzkolenia(szkolenie.miastoSzkolenia || "");
+      setUlicaSzkolenia(szkolenie.ulicaSzkolenia || "");
+      setNumerMieszkaniaSzkolenia(szkolenie.numerMieszkaniaSzkolenia || "");
+      setNumerUmowySzkolenia(szkolenie.numerUmowySzkolenia || "");
+      setDataUmowySzkolenia(szkolenie.dataUmowySzkolenia || "");
+      setUmowaPowierdzaniaDanych(szkolenie.umowaPowierdzaniaDanych || "");
+      setUmowaZgloszonaWup(szkolenie.umowaZgloszonaWup || "");
+      setDataZgloszenia(szkolenie.dataZgloszenia || "");
+      setNazwaFrimyPodwykonawcy(szkolenie.nazwaFrimyPodwykonawcy || "");
+      setKodPocztowyPodwykonawcy(szkolenie.kodPocztowyPodwykonawcy || "");
+      setMiastoPodwykonawcy(szkolenie.miastoPodwykonawcy || "");
+      setUlicaPodwykonawcy(szkolenie.ulicaPodwykonawcy || "");
+      setNumerMieszkaniaPodwykonawcy(
+        szkolenie.numerMieszkaniaPodwykonawcy || ""
+      );
+      setTrener(szkolenie.trener || "");
+      setEgzaminator(szkolenie.egzaminator || "");
+      setTerminSzkoleniaOd(szkolenie.terminSzkoleniaOd || "");
+      setTerminSzkoleniaDo(szkolenie.terminSzkoleniaDo || "");
+      setRekomendacje(szkolenie.rekomendacje || "");
+    }
+  }, [szkolenie]);
+
   return (
     <Form>
       <FormLabel>
         Nazwa szkolenia:
-        <FormInput></FormInput>
+        <FormInput
+          value={nazwaSzkolenia}
+          onChange={(e) => setNazwaSzkolenia(e.target.value)}
+        ></FormInput>
       </FormLabel>
       <FormLabel>
         Numer grupy:
-        <FormInput></FormInput>
+        <FormInput
+          value={numerGrupy}
+          onChange={(e) => setNumerGrupy(e.target.value)}
+        ></FormInput>
       </FormLabel>
       <FormHeader>Wykonawca szkolenia:</FormHeader>
       <FormLabel>
         Nazwa firmy:
-        <FormInput></FormInput>
+        <FormInput
+          value={nazwaFrimySzkolenia}
+          onChange={(e) => setNazwaFrimySzkolenia(e.target.value)}
+        ></FormInput>
       </FormLabel>
       <FormLabel>
         Kod pocztowy
-        <FormInput type="number"></FormInput>
+        <FormInput
+          value={kodPocztowySzkolenia}
+          onChange={(e) => setKodPocztowySzkolenia(e.target.value)}
+          type="number"
+        ></FormInput>
       </FormLabel>
       <FormLabel>
         Miasto:
-        <FormInput></FormInput>
+        <FormInput
+          value={miastoSzkolenia}
+          onChange={(e) => setMiastoSzkolenia(e.target.value)}
+        ></FormInput>
       </FormLabel>
       <FormLabel>
         Ulica
-        <FormInput></FormInput>
+        <FormInput
+          value={ulicaSzkolenia}
+          onChange={(e) => setUlicaSzkolenia(e.target.value)}
+        ></FormInput>
       </FormLabel>
       <FormLabel>
         Numer mieszkania:
-        <FormInput type="number"></FormInput>
+        <FormInput
+          value={numerMieszkaniaSzkolenia}
+          onChange={(e) => setNumerMieszkaniaSzkolenia(e.target.value)}
+          type="number"
+        ></FormInput>
       </FormLabel>
       <br />
       <FormLabel>
         Numer umowy:
-        <FormInput type="number"></FormInput>
+        <FormInput
+          value={numerUmowySzkolenia}
+          onChange={(e) => setNumerUmowySzkolenia(e.target.value)}
+          type="number"
+        ></FormInput>
       </FormLabel>
       <FormLabel>
         Data umowy:
-        <FormInput type="date"></FormInput>
+        <FormInput
+          value={dataUmowySzkolenia}
+          onChange={(e) => setDataUmowySzkolenia(e.target.value)}
+          type="date"
+        ></FormInput>
       </FormLabel>
       <FormLabel>
         Umowa powierzania danych:
-        <FormSelect>
+        <FormSelect
+          value={umowaPowierdzaniaDanych}
+          onChange={(e) => setUmowaPowierdzaniaDanych(e.target.value)}
+        >
           <FormOption>Nie</FormOption>
           <FormOption>Tak</FormOption>
         </FormSelect>
       </FormLabel>
       <FormLabel>
         Umowa zgłoszona do WUP:
-        <FormSelect>
+        <FormSelect
+          value={umowaZgloszonaWup}
+          onChange={(e) => setUmowaZgloszonaWup(e.target.value)}
+        >
           <FormOption>Nie</FormOption>
           <FormOption>Tak</FormOption>
         </FormSelect>
       </FormLabel>
       <FormLabel>
         Data zgłoszenia:
-        <FormInput type="date"></FormInput>
+        <FormInput
+          value={dataZgloszenia}
+          onChange={(e) => setDataZgloszenia(e.target.value)}
+          type="date"
+        ></FormInput>
       </FormLabel>
       <FormHeader>Podwykonawca szkolenia:</FormHeader>
       <FormLabel>
         Nazwa firmy:
-        <FormInput></FormInput>
+        <FormInput
+          value={nazwaFrimyPodwykonawcy}
+          onChange={(e) => setNazwaFrimyPodwykonawcy(e.target.value)}
+        ></FormInput>
       </FormLabel>
       <FormLabel>
         Kod pocztowy
-        <FormInput type="number"></FormInput>
+        <FormInput
+          value={kodPocztowyPodwykonawcy}
+          onChange={(e) => setKodPocztowyPodwykonawcy(e.target.value)}
+          type="number"
+        ></FormInput>
       </FormLabel>
       <FormLabel>
         Miasto:
-        <FormInput></FormInput>
+        <FormInput
+          value={miastoPodwykonawcy}
+          onChange={(e) => setMiastoPodwykonawcy(e.target.value)}
+        ></FormInput>
       </FormLabel>
       <FormLabel>
         Ulica
-        <FormInput></FormInput>
+        <FormInput
+          value={ulicaPodwykonawcy}
+          onChange={(e) => setUlicaPodwykonawcy(e.target.value)}
+        ></FormInput>
       </FormLabel>
       <FormLabel>
         Numer mieszkania:
-        <FormInput type="number"></FormInput>
+        <FormInput
+          value={numerMieszkaniaPodwykonawcy}
+          onChange={(e) => setNumerMieszkaniaPodwykonawcy(e.target.value)}
+          type="number"
+        ></FormInput>
       </FormLabel>
       <br />
       <FormLabel>
         Dodaj trenera:
-        <FormSelect>
+        <FormSelect value={trener} onChange={(e) => setTrener(e.target.value)}>
           {data.trener.map((item) => (
-            <FormOption>{item}</FormOption>
+            <FormOption key={item}>{item}</FormOption>
           ))}
         </FormSelect>
       </FormLabel>
       <FormLabel>
         Dodaj egzaminatora:
-        <FormSelect>
+        <FormSelect
+          value={egzaminator}
+          onChange={(e) => setEgzaminator(e.target.value)}
+        >
           {data.egzaminator.map((item) => (
-            <FormOption>{item}</FormOption>
+            <FormOption key={item}>{item}</FormOption>
           ))}
         </FormSelect>
       </FormLabel>
@@ -169,17 +312,28 @@ const Training = () => {
         <FormDateContainer>
           <FromDateWrapper>
             <FormSpan>od:</FormSpan>
-            <FormInput type="date"></FormInput>
+            <FormInput
+              value={terminSzkoleniaOd}
+              onChange={(e) => setTerminSzkoleniaOd(e.target.value)}
+              type="date"
+            ></FormInput>
           </FromDateWrapper>
           <FromDateWrapper>
             <FormSpan>do:</FormSpan>
-            <FormInput type="date"></FormInput>
+            <FormInput
+              value={terminSzkoleniaDo}
+              onChange={(e) => setTerminSzkoleniaDo(e.target.value)}
+              type="date"
+            ></FormInput>
           </FromDateWrapper>
         </FormDateContainer>
       </FormLabel>
       <FormLabel>
         Rekomendacje:
-        <FormSelect>
+        <FormSelect
+          value={rekomendacje}
+          onChange={(e) => setRekomendacje(e.target.value)}
+        >
           <FormOption>Nie</FormOption>
           <FormOption>Tak</FormOption>
         </FormSelect>
@@ -188,7 +342,12 @@ const Training = () => {
         <Button variant="outlined" className={classes.button}>
           Zakończ szkolenie
         </Button>
-        <Button type="submit" variant="outlined" className={classes.button}>
+        <Button
+          onClick={handleSubmit}
+          type="submit"
+          variant="outlined"
+          className={classes.button}
+        >
           Zapisz
         </Button>
       </FormFlexContainer>
